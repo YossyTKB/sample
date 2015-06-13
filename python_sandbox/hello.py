d81d3954b4dc041ec86162361029109e35bc7614
Yoshitaka Takebe <yossy.tkb@gmail.com>
2015-06-13 14:40:27 +0900

    追加

---
+++ python_sandbox/hello.py
@@ -0,0 +1,31 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      yoshitaka
+#
+# Created:     27/05/2014
+# Copyright:   (c) yoshitaka 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import platform
+
+def main():
+    print 'hello'
+    print 'Version      :', platform.python_version()
+    print 'Version tuple:', platform.python_version_tuple()
+    print 'Compiler     :', platform.python_compiler()
+    print 'Build        :', platform.python_build()
+
+if __name__ == '__main__':
+    main()
+    print 'test'
+    print 'test'
+    i=1
+    if (i!=1):
+        pass
+    else:
+        pass
+
+
+
